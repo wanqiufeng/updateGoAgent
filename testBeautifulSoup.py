7c651160d20cdb4df41e31e5cff213ee4d6a22e7
wanqiufeng <waqiufeng@outlook.com>
2013-11-05 18:12:46 +0800

    add code to obtain remote version's version no and download address.

--- testBeautifulSoup.py
+++ testBeautifulSoup.py
@@ -1,1 +1,19 @@
 __author__ = 'vincentc'
+import bs4
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+soup = bs4.BeautifulSoup(html_doc)
+
+print(soup.title.string)
--- updateGoAgent.py
+++ updateGoAgent.py
@@ -3,37 +3,60 @@ import sys
 import requests
 import re
 import bs4
-global newVersionNo
+
+
+global remoteVersionNo
 global downloadAddr
+
+
 def downloadNewVersion():
     print("new version download")
+
+
 def replaceOldVersion():
     pass
+
+
 def addAppID():
     pass
+
+
 def deploy():
     pass
+
+
 def hasNewVersion():
-    if getLocalVersion() != getRemoteVersion():
+    if getLocalVersion():
         return True
     return False
 
+
 def getLocalVersion():
     with open('local/proxy.py', 'rt') as f:
-            for line in f:
-                if "__version__" in line:
-                    return(eval(line[line.index("=")+1:]))
+        for line in f:
+            if "__version__" in line:
+                return (eval(line[line.index("=") + 1:]))
+
 
 def getRemoteVersionInfo():
     r = requests.get('https://code.google.com/p/goagent/')
-    result = r.text
-    match = re.search('<p>(.*)</p>',result)
-    matchVersion = re.search('goagent (.+?) 正式版下载',match.group(0))
-    newVersionNo = matchVersion.group(1)
+    soup = bs4.BeautifulSoup(r.text)
 
+    #get remote version no
+    remoteVersionNoStr = str(soup.p.find(text=True))
+    matchVersion = re.search('goagent (.+?) 正式版下载',remoteVersionNoStr)
+    remoteVersionNo = matchVersion.group(1)
+    print(remoteVersionNo)
+
+    #get remote version download address
+    downloadAddr = str(soup.p.a['href'])
+    print(downloadAddr)
 
 def main():
+    getRemoteVersionInfo()
     if hasNewVersion():
         downloadNewVersion()
+
+
 if __name__ == '__main__':
-   main()
+    main()
